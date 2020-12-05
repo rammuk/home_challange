a3b8e8793b894c4039e7b45905c12982becea6b3
Ram Mukmel <ram.mukmel@gmail.com>
2020-12-05 15:04:56 +0100

    Init app: Fresh install + welcome page

---
+++ README.old.md
@@ -0,0 +1,1 @@
+#Home fronend Challange
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">      
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Frontend challange by Ram Mukmel
+        </p>
+        <a
+          className="btn"
+          href="#"          
+          rel="noopener noreferrer"
+        >
+          Start
+        </a>      
+    </div>
+  );
+}
+
+export default App;
